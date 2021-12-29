1e2afa0fd467fa4d65445ccec1ab83a36a6fd8e6
Eduardo <eduardo.s.tanaka@gmail.com>
2021-12-29 12:54:28 -0300

    bottombar e paginas

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,21 +1,90 @@
 import 'package:flutter/material.dart';
-import 'package:picpay/widgets/bottom_bar/bottom_bar_widget.dart';
+import 'package:picpay/modules/carteira/carteira_page.dart';
+import 'package:picpay/modules/inicio/inicio_page.dart';
+import 'package:picpay/modules/notificacoes/notificacaoes_page.dart';
+import 'package:picpay/modules/pagar/pagar_page.dart';
+import 'package:picpay/modules/store/store_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    InicioPage(),
+    CarteiraPage(),
+    PagarPage(),
+    NotificacoesPage(),
+    StorePage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        child: Text("Home"),
+      body: Container(child: _widgetOptions.elementAt(_selectedIndex)),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        iconSize: 28,
+        selectedItemColor: Colors.green[900],
+        selectedLabelStyle: TextStyle(
+          color: Colors.black,
+          fontSize: 12,
+        ),
+        unselectedItemColor: Colors.black87,
+        unselectedLabelStyle: TextStyle(
+          color: Colors.black,
+        ),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+            ),
+            label: 'Início',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet_outlined),
+            label: 'Carteira',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(null),
+            label: 'Pagar',
+            tooltip: 'Pagar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications_outlined),
+            label: 'Notificações',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag_outlined),
+            label: 'Store',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
-      bottomNavigationBar: BottomBarWidget(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
+        child: Icon(
+          Icons.attach_money,
+        ),
+        onPressed: () {
+          setState(() {
+            _selectedIndex = 2;
+          });
+        },
+        backgroundColor: _selectedIndex == 2 ? Colors.green[900] : Colors.green,
       ),
     );
   }
---
+++ lib/modules/inicio/inicio_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InicioPage extends StatelessWidget {
+  const InicioPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
